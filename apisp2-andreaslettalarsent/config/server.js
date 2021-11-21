643881c6861d224ec8c8b9e050803d34e6f02d77
AndreaSletta <70203798+AndreaSletta@users.noreply.github.com>
2021-11-21 09:19:59 +0100

    a

---
+++ apisp2-andreaslettalarsent/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'da35ebcf22162a54b0ce5c612ec500b7'),
+    },
+  },
+});
---
+++ apisp2-andreaslettalarsent/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ef398579-88a1-41d3-a264-324d3918ea39'
+};
